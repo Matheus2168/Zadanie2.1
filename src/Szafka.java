e49398894780e4dec7606649f2ec106c8bbe4f4d
Mateusz <mateuszhotowy@gmail.com>
2017-11-28 10:31:44 +0100

    Pierwsza część zadania

--- src/Szafka.java
+++ src/Szafka.java
@@ -6,4 +6,17 @@ public class Szafka {
     int numberOfDrawers;
     boolean isItWooden;
 
+
+
+    Szafka(String fullName,double height,double width,int numberOfDrawers,boolean isItWooden){
+
+        this.fullName = fullName;
+        this.height = height;
+        this.width = width;
+        this.numberOfDrawers = numberOfDrawers;
+        this.isItWooden = isItWooden;
+
+    }
+
+
 }
